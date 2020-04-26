431748f8b3dd09fc2272507f2c4e923d411ff35a
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-04-26 14:01:56 +0530

    Added ThemeContext and wired with app screens to use DarkTheme and defaultTheme switch

--- src/screens/UserStartingScreen/index.js
+++ src/screens/UserStartingScreen/index.js
@@ -1,12 +1,47 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Text, Switch} from 'react-native-paper';
+
+import {Context as ThemeContext} from '../../contexts/ThemeContext';
+
+// importing styles
+import styles from './styles';
 
 class UserStartingScreen extends Component {
+  static contextType = ThemeContext;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSwitchOn: false,
+    };
+  }
+
+  componentDidMount() {
+    const themeContext = this.context;
+    console.log(themeContext);
+  }
+
+  _onToggleSwitch = () => {
+    const {isSwitchOn} = this.state;
+    const {enableDarkTheme, disableDarkTheme} = this.context;
+    if (isSwitchOn) {
+      disableDarkTheme();
+    } else {
+      enableDarkTheme();
+    }
+    this.setState({
+      isSwitchOn: !isSwitchOn,
+    });
+  };
+
   render() {
+    const {isSwitchOn} = this.state;
+
     return (
-      <View>
+      <View style={styles.userStartingContainer}>
         <Text>This is User Starting Screen</Text>
+        <Switch value={isSwitchOn} onValueChange={this._onToggleSwitch} />
       </View>
     );
   }
---
+++ src/screens/UserStartingScreen/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  userStartingContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
