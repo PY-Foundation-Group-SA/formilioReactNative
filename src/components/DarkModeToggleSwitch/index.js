bef72436e6d421cb262524067041a497468677c1
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-04-26 14:30:53 +0530

    refactored new DarkModeToggleSwitch to display a respective icon

--- src/components/DarkModeToggleSwitch/index.js
+++ src/components/DarkModeToggleSwitch/index.js
@@ -1,13 +1,14 @@
 import React, {useContext} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Switch} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Feather';
 
 // importing context for theme preference
 import {Context as ThemeContext} from '../../contexts/ThemeContext';
 
 const DarkModeToggleSwitch = () => {
   const {state, enableDarkTheme, disableDarkTheme} = useContext(ThemeContext);
-
+  console.log(state);
   const _onToggleSwitch = () => {
     if (state[0]) {
       disableDarkTheme();
@@ -17,10 +18,20 @@ const DarkModeToggleSwitch = () => {
   };
 
   return (
-    <>
+    <View style={styles.toggleButtonContainer}>
       <Switch value={!!state[0]} onValueChange={_onToggleSwitch} />
-    </>
+      <Icon name={state[0] ? 'moon' : 'sun'} size={24} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  toggleButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default DarkModeToggleSwitch;
