7765940540fbd85e9fdcd8a27b31925b346e4480
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-05-13 13:32:28 +0530

    Tweaking the upload-apk script

--- upload-apk.sh
+++ upload-apk.sh
@@ -8,14 +8,14 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   cd $HOME/Github/
   git config --global user.email "sarthak.pranesh2018@vitstudent.ac.in" 
   git config --global user.name "sarthakpranesh" 
-  git clone https://sarthakpranesh:$GITHUB_API_KEY@github.com/sarthakpranesh/formilioReactNative.git
 
+  mkdir formilioReactNative
   cd formilioReactNative
+  git init
+  git remote add origin https://sarthakpranesh:$GITHUB_API_KEY@github.com/sarthakpranesh/formilioReactNative.git
   git checkout -b build
-  rm -rf ./* .*
   cp -Rf $HOME/buildApk/* .
   git add .
-  git remote add origin https://sarthakpranesh:$GITHUB_API_KEY@github.com/sarthakpranesh/formilioReactNative.git
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER push" 
   git push origin build -f
   echo "Done"
